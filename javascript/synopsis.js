c85b00cfc11777af0a1ef98deabb42cce1478d55
Ricardo do Carmo <therick0156@gmail.com>
2026-06-13 17:14:21 -0300

    update: adcionando Porco Rosso, Homem Aranha

--- javascript/synopsis.js
+++ javascript/synopsis.js
@@ -6,9 +6,36 @@ const movies = {
     studio: "Studio Ghibli",
     imdb: "8.2/10",
     tomato: "88%",
-    poster: "img/film-img/CasteloAnimado.jpg",
+    poster: "/img/film-img/CasteloAnimado.jpg",
     trailer: "https://www.youtube.com/embed/2x5SejvTMeA",
     synopsis:
       "Sophie é uma jovem chapeleira que leva uma vida tranquila até ser amaldiçoada por uma bruxa e transformada em uma senhora idosa. Em busca de uma forma de quebrar o feitiço, ela encontra o misterioso mago Howl e passa a viver em seu castelo ambulante. Juntos, eles enfrentam magia, conflitos e descobrem sentimentos que mudam suas vidas.",
   },
+
+  porco: {
+        title: "Kurenai no Buta (Porco Rosso)",
+        year: 1992,
+        director: "Hayao Miyazaki",
+        studio: "Studio Ghibli",
+        imdb: "7,7/10",
+        tomato: "96%",
+        trailer: "https://www.youtube.com/embed/3LRyNwYg1f8?si=LgkJenxhKZE4rmKV",
+        poster: "/img/film-img/PorcoRosso.jpg",
+
+        synopsis: "A história se passa no Mar Adriático entre as duas guerras mundiais. Marco Pagot é um ex-piloto de caça que, por uma misteriosa maldição, foi transformado em um porco antropomórfico. Conhecido como Porco Rosso, ele trabalha como caçador de recompensas e protege os céus dos piratas do ar. Quando um piloto americano desafia sua reputação, Porco embarca em uma aventura repleta de ação, amizade e reflexões sobre liberdade e identidade.",
+    },
+
+    spider_man: {
+        title: "Spider-Man: Into the Spider-Verse (Homem-Aranha no Aranhaverso)",
+        year: 2018,
+        director: "Bob Persichetti, Peter Ramsey e Rodney Rothman",
+        studio: " Sony Pictures Animation",
+        imdb: "8,4/10",
+        tomato: "93%",
+        trailer: "https://www.youtube.com/embed/he5SXkqxoE4?si=ht8k_m_jVpfE3T92",
+        poster: "/img/film-img/spider-man-into-the-spider.jpg",
+
+        synopsis: "Miles Morales, um adolescente do Brooklyn, é mordido por uma aranha radioativa e adquire poderes semelhantes aos do Homem-Aranha. Quando um experimento interdimensional dá errado, diferentes versões do Homem-Aranha de universos paralelos chegam ao seu mundo. Juntos, eles precisam impedir uma ameaça que coloca em risco todas as realidades enquanto Miles aprende o que significa ser um herói.",
+    },
+
 };
