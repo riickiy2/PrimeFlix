e7ffb00696d584822ad43577ccc3c42d864a858b
Ricardo do Carmo <therick0156@gmail.com>
2026-06-13 18:40:22 -0300

    update: corrigindo caminho do Poster para GitHub

--- javascript/synopsis.js
+++ javascript/synopsis.js
@@ -6,7 +6,7 @@ const movies = {
     studio: "Studio Ghibli",
     imdb: "8.2/10",
     tomato: "88%",
-    poster: "/img/film-img/CasteloAnimado.jpg",
+    poster: "../img/film-img/CasteloAnimado.jpg",
     trailer: "https://www.youtube.com/embed/2x5SejvTMeA",
     synopsis:
       "Sophie é uma jovem chapeleira que leva uma vida tranquila até ser amaldiçoada por uma bruxa e transformada em uma senhora idosa. Em busca de uma forma de quebrar o feitiço, ela encontra o misterioso mago Howl e passa a viver em seu castelo ambulante. Juntos, eles enfrentam magia, conflitos e descobrem sentimentos que mudam suas vidas.",
@@ -20,7 +20,7 @@ const movies = {
         imdb: "7,7/10",
         tomato: "96%",
         trailer: "https://www.youtube.com/embed/3LRyNwYg1f8?si=LgkJenxhKZE4rmKV",
-        poster: "/img/film-img/PorcoRosso.jpg",
+        poster: "../img/film-img/PorcoRosso.jpg",
 
         synopsis: "A história se passa no Mar Adriático entre as duas guerras mundiais. Marco Pagot é um ex-piloto de caça que, por uma misteriosa maldição, foi transformado em um porco antropomórfico. Conhecido como Porco Rosso, ele trabalha como caçador de recompensas e protege os céus dos piratas do ar. Quando um piloto americano desafia sua reputação, Porco embarca em uma aventura repleta de ação, amizade e reflexões sobre liberdade e identidade.",
     },
@@ -33,7 +33,7 @@ const movies = {
         imdb: "8,4/10",
         tomato: "93%",
         trailer: "https://www.youtube.com/embed/he5SXkqxoE4?si=ht8k_m_jVpfE3T92",
-        poster: "/img/film-img/spider-man-into-the-spider.jpg",
+        poster: "../img/film-img/spider-man-into-the-spider.jpg",
 
         synopsis: "Miles Morales, um adolescente do Brooklyn, é mordido por uma aranha radioativa e adquire poderes semelhantes aos do Homem-Aranha. Quando um experimento interdimensional dá errado, diferentes versões do Homem-Aranha de universos paralelos chegam ao seu mundo. Juntos, eles precisam impedir uma ameaça que coloca em risco todas as realidades enquanto Miles aprende o que significa ser um herói.",
     },
@@ -46,7 +46,7 @@ const movies = {
         imdb: "7,3/10",
         tomato: "79%",
         trailer: "https://www.youtube.com/embed/dxWvtMOGAhw?si=0UwKL5Fy_bSIx7tH",
-        poster: "/img/film-img/black-panther.jpg",
+        poster: "../img/film-img/black-panther.jpg",
 
         synopsis: "Após a morte de seu pai, o rei de Wakanda, T'Challa retorna à sua nação africana altamente desenvolvida para assumir o trono. Porém, um inimigo ligado ao passado do país surge e ameaça não apenas Wakanda, mas também o mundo. Para proteger seu povo, T'Challa precisa unir seus aliados e assumir plenamente o papel de Pantera Negra.",
     },
@@ -59,7 +59,7 @@ const movies = {
         imdb: "8,1/10",
         tomato: "90%",
         trailer: "https://www.youtube.com/embed/MmoBvmJA9XI?si=glla1OznwuIil9CI" ,
-        poster: "/img/film-img/i-want-to-eat-your-pancreas.jpg",
+        poster: "../img/film-img/i-want-to-eat-your-pancreas.jpg",
 
         synopsis: "A história acompanha um estudante introvertido que encontra o diário de sua colega de classe, Sakura Yamauchi. Ao ler o diário, ele descobre que ela sofre de uma doença terminal no pâncreas e tem pouco tempo de vida. Apesar de suas personalidades opostas, os dois desenvolvem uma amizade profunda que transforma a maneira como enxergam a vida, os relacionamentos e o valor do tempo.",
     },
@@ -72,7 +72,7 @@ const movies = {
         imdb: "7,4/10",
         tomato: "73%",
         trailer: "https://www.youtube.com/embed/T0MmkG_nG1U?si=DS0Rt5k-hPIo3YA4" ,
-        poster: "/img/film-img/me-before-you.jpg",
+        poster: "../img/film-img/me-before-you.jpg",
 
         synopsis: "Louisa Clark é uma jovem alegre e espontânea que aceita o emprego de cuidadora de Will Traynor, um ex-banqueiro bem-sucedido que ficou tetraplégico após um acidente. Apesar das diferenças entre eles, os dois desenvolvem uma forte conexão que transforma suas vidas. Ao longo da convivência, Louisa tenta mostrar a Will que a vida ainda pode oferecer novas experiências e motivos para seguir em frente.",
     },
@@ -85,7 +85,7 @@ const movies = {
         imdb: "8,7/10",
         tomato: "87%",
         trailer: "https://www.youtube.com/embed/i6avfCqKcQo?si=J-3aVuSu7GQwg_De",
-        poster: "/img/film-img/interstellar.jpg",
+        poster: "../img/film-img/interstellar.jpg",
 
         synopsis: "Em um futuro próximo, a Terra enfrenta uma crise ambiental que ameaça a sobrevivência da humanidade. Cooper, um ex-piloto da NASA e engenheiro, é recrutado para uma missão interestelar através de um buraco de minhoca próximo a Saturno. Junto com uma equipe de cientistas, ele busca planetas habitáveis para garantir o futuro da espécie humana, enquanto lida com os efeitos da relatividade do tempo e a distância de sua família.",
     },
@@ -98,7 +98,7 @@ const movies = {
         imdb: "8,0/10",
         tomato: "90%",
         trailer: "https://www.youtube.com/embed/Q9X-bAE8KTc?si=ybhXz3zYaN2m8huE",
-        poster: "/img/film-img/deadpool.jpg",
+        poster: "../img/film-img/deadpool.jpg",
 
         synopsis: "Wade Wilson é um ex-militar e mercenário que descobre ter câncer terminal. Desesperado por uma cura, ele se submete a um experimento secreto que lhe concede poderes de regeneração extraordinários, mas também deixa seu corpo desfigurado. Adotando a identidade de Deadpool, ele passa a caçar os responsáveis por sua transformação enquanto utiliza seu humor ácido e quebra constantemente a quarta parede.",
     },
@@ -111,7 +111,7 @@ const movies = {
         imdb: "7,4/10",
         tomato: "94%",
         trailer: "https://www.youtube.com/embed/eil_ZV9P8AY?si=hXy9bhStNXYoNpmW",
-        poster: "/img/film-img/entergalactic.jpg",
+        poster: "../img/film-img/entergalactic.jpg",
 
         synopsis: "A história acompanha Jabari, um jovem artista talentoso que está prestes a alcançar o sucesso em Nova York. Ao se mudar para um novo apartamento, ele conhece sua vizinha Meadow, uma fotógrafa criativa e independente. Conforme os dois desenvolvem uma conexão especial, precisam equilibrar amor, carreira, amizades e os desafios da vida adulta.",
     },
@@ -124,8 +124,8 @@ const movies = {
         imdb: "7,2/10",
         tomato: "67%",
         trailer: "https://www.youtube.com/embed/ZzirthtD7kM?si=mi37dkPCxq8Eporl" ,
-        poster: "/img/film-img/the-age-of-adaline.jpg",
-        
+        poster: "../img/film-img/the-age-of-adaline.jpg",
+
         synopsis: "Adaline Bowman leva uma vida aparentemente comum até sofrer um acidente que a impede de envelhecer. Permanecendo com a aparência de 29 anos durante décadas, ela vive isolada e muda constantemente de identidade para esconder seu segredo. Sua vida muda quando conhece Ellis Jones, um homem por quem se apaixona, forçando-a a escolher entre continuar escondida ou finalmente viver o amor e enfrentar as consequências de sua condição.",
     },
 
